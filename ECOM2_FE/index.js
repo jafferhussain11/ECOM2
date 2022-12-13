170954fa32f7d028fcc699e6f69a848a0aa637e2
jafferhussain11 <jafferhussain11@gmail.com>
2022-12-13 19:27:07 +0530

    cart with backend DB

--- ECOM2_FE/index.js
+++ ECOM2_FE/index.js
@@ -1,14 +1,14 @@
 
 const btn = document.getElementById('btn');
 const cart = document.getElementById('cart');
-
+let productsList = [];
 const cart_items = document.querySelector('.cart-items');
 window.addEventListener('load', () => {
 
     console.log('loaded');
     axios.get('http://localhost:5000/products').then((products) => {
 
-        let productsList = products.data;
+        productsList = products.data;
         let output = '';
         productsList.forEach((product) => {
             output += `
@@ -21,52 +21,49 @@ window.addEventListener('load', () => {
                     <h4 class="itemPrice">${product.price}</h4>
                 </div>
                 <div class="shop-item-button">
-                    <button>Add to cart</button>
+                    <button id='addbutn'>Add to cart</button>
+                </div>
+                <div class="itemid">
+                    <h4 hidden>${product.id}</h4>
                 </div>
             </div>
             `;
         });
         document.getElementById('maincourse').children[1].innerHTML = output;
         //console.log(output);
-        console.log(products);
+        //console.log(products);
         
     }).catch(err=>console.log(err));
 });
 
 btn.addEventListener('click', () => {
+    
+    
     cart.classList.toggle('active');
     btn.classList.toggle('active');
+    axios.get('http://localhost:5000/cart').then((cartProducts) => {
+        console.log(cartProducts);
+        showCartProducts(cartProducts.data);
+    }).catch(err => console.log(err));
 });
 
 document.addEventListener('click', (e) => {
 
-    if(e.target.className == 'shop-item-button') {
+    if(e.target.id == 'addbutn') {
        
-        let item = e.target.parentElement.parentElement; // item is the div with class shop-item or addtocart button
-        let title = item.querySelector('.shop-item-title').textContent;
-        let price = item.querySelector('.itemPrice').textContent;
-        let img = item.children[1].src;
-
-        let newItem = document.createElement('div');
-        newItem.classList.add('cart-item');
-        newItem.innerHTML = `
-            
-            <div class="cart-column cart-item-info">
-                <img class="cartimg" src="${img}" alt="">
-                <h4>${title}</h4>
-            </div>
-            <div class="cart-column cart-price">
-                <h4>${price}</h4>
-            </div>
-
-            <button class="cart-item-button">Remove</button>
-        `;
-
-        cart_items.appendChild(newItem);
+        let title = e.target.parentElement.parentElement.children[1].children[0].textContent;
+        let id = Number(document.querySelector('.itemid').textContent);
+        axios.post('http://localhost:5000/cart', {
+            id: id,
+        }).then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
         showNotification(`Item ${title} was added to cart`, false);
 
 
+
     }
+   
 });
 function showNotification(message, iserror){
     const container = document.getElementById('noti-container');
@@ -80,3 +77,30 @@ function showNotification(message, iserror){
     },2500)
 }
 
+function showCartProducts(cartProducts) {
+    
+ cart_items.innerHTML = "";
+ cartProducts.forEach((product) => {
+
+
+    let title = product.title;
+    let price = product.price;
+    let img = product.imageUrl;
+    //console.log(img);
+    let newItem = document.createElement('div');
+    newItem.classList.add('cart-item');
+    newItem.innerHTML = `
+        
+        <div class="cart-column cart-item-info">
+            <img class="cartimg" src="${img}" alt="">
+            <h4>${title}</h4>
+        </div>
+        <div class="cart-column cart-price">
+            <h4>${price}</h4>
+        </div>
+
+        <button class="cart-item-button">Remove</button>
+    `;
+    cart_items.appendChild(newItem);
+ });
+}
